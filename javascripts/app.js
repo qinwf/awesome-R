7fd37ebbc2460380d6b23a9b32110a4b83a9990e
qinwf <mail@qinwenfeng.com>
2016-04-13 18:41:47 +0800

    Add: more button

--- javascripts/app.js
+++ javascripts/app.js
@@ -94,3 +94,24 @@ $(document).ready(function(){
     }
   };
 })(jQuery);
+
+function get_vars(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
+
+function getpages(user,files){
+    window.history.pushState(window.history.state,null, window.location.origin + window.location.pathname + '?repo=' +encodeURIComponent(user)+ '&file='+encodeURIComponent(files))
+    Flatdoc.run({
+      fetcher: Flatdoc.github(user,files)
+    });
+}
+
+
